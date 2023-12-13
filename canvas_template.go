6d9d0ec3a869049b68557841bd66be902a195e34
junyu.wang <junyu.wang@bytedance.com>
2023-12-13 13:22:07 -0800

    add

--- canvas_template.go
+++ canvas_template.go
@@ -3,18 +3,8 @@ package main
 // Define the structures based on the JSON input
 const (
 	RelatedTicketID = "related-ticket"
-	CreateTicketID  = "create-ticket"
 	SubmitTicketID  = "submit-ticket"
 )
-
-type InputValues struct {
-	TicketOption string `json:"pre-oncall-ticket-option"`
-}
-
-type InitializeRequest struct {
-	CurrentCanvas Canvas      `json:"current_canvas"`
-	InputValues   InputValues `json:"input_values"`
-}
 
 type Workspace struct {
 	WorkspaceID     string       `json:"workspace_id"`
