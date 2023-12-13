b241a7a60a91ecc52875cd42a345ccfa80d62220
junyu.wang <junyu.wang@bytedance.com>
2023-12-13 15:46:32 -0800

    save values

--- canvasRouter.go
+++ canvasRouter.go
@@ -1,5 +1,40 @@
 package main
 
-func handlePreoncallCanvasSubmitAction(action string) {
+import (
+	"encoding/json"
+	"fmt"
+	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
+	"log"
+)
 
+func HandlePreoncallCanvasSubmitAction(body string) (CanvasReponse, error) {
+	var initReq Workspace
+	if err := json.Unmarshal([]byte(body), &initReq); err != nil {
+		fmt.Println("meet parse err", err)
+
+		return CanvasReponse{}, err
+	}
+	fmt.Println("pretty's request %v", larkcore.Prettify(initReq))
+	fmt.Println("======================================")
+	response := GetCreateTicketCanvasBody([]string{"bizline1", "bizline2"}, []string{"region1", "region2"}, []string{"stack1", "stack2"}, initReq.InputValues)
+	//fmt.Println("GetCreateTicketCanvasBody response %v", larkcore.Prettify(response))
+	// Send the response as JSON
+	// Marshal the struct to a JSON byte slice
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		log.Fatalf("Error occurred during marshaling. Error: %s", err.Error())
+	}
+
+	// Convert the byte slice to a string and print it
+	jsonString := string(jsonData)
+	fmt.Println("this is the submit json string %v", jsonString)
+
+	return response, nil
+}
+
+func HandlePreoncallInitializationAction() CanvasReponse {
+	// Construct the response object
+	response := GetInitTicketCanvasBody()
+
+	return response
 }
--- requestHandler.go
+++ requestHandler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 	"log"
 
 	"io/ioutil"
@@ -17,36 +16,24 @@ func SubmitHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Println("-------------------------------------- this is submit request")
+	defer r.Body.Close()
 	// Convert the bytes to string and print it
 	bodyString := string(bodyBytes)
 	fmt.Println("this iis the body this %v", bodyString)
 	fmt.Println("======================================")
 	// You must close the original body
-	defer r.Body.Close()
+
 	// Unmarshal the JSON into your struct
-	var initReq Workspace
-	if err := json.Unmarshal([]byte(bodyString), &initReq); err != nil {
-		fmt.Println("meet parse err", err)
+	response, err := HandlePreoncallCanvasSubmitAction(bodyString)
+	if err != nil {
+		//log.Fatalf("Error occurred during marshaling. Error: %s", err.Error())
 		http.Error(w, "Error unmarshalling request body", http.StatusBadRequest)
 		return
 	}
-	fmt.Println("pretty's request %v", larkcore.Prettify(initReq))
-	fmt.Println("======================================")
-	response := GetCreateTicketCanvasBody([]string{"bizline1", "bizline2"}, []string{"region1", "region2"}, []string{"stack1", "stack2"}, initReq.InputValues)
-	//fmt.Println("GetCreateTicketCanvasBody response %v", larkcore.Prettify(response))
-	// Send the response as JSON
-	// Marshal the struct to a JSON byte slice
-	jsonData, err := json.Marshal(response)
-	if err != nil {
-		log.Fatalf("Error occurred during marshaling. Error: %s", err.Error())
-	}
-
-	// Convert the byte slice to a string and print it
-	jsonString := string(jsonData)
-	fmt.Println("this is the submit json string %v", jsonString)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+
 }
 
 func InitializeCanvasHandler(w http.ResponseWriter, r *http.Request) {
@@ -60,10 +47,7 @@ func InitializeCanvasHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Log the body, can remove this in production
 	fmt.Println("Received initialize request with body:", string(body))
-
-	// Construct the response object
-	response := GetInitTicketCanvasBody()
-
+	response := HandlePreoncallInitializationAction()
 	fmt.Println("response %v", response)
 
 	jsonData, err := json.Marshal(response)
