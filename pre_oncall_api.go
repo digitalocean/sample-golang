b4fc17628dbdbaaaa760178ecb975601ba1cf4f4
junyu.wang <junyu.wang@bytedance.com>
2023-12-15 15:24:40 -0800

    add metainfo struct

--- pre_oncall_api.go
+++ pre_oncall_api.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
 // Define a struct to match the JSON request body
-type requestBody struct {
+type MetaInfoApiRequestBody struct {
 	Business      bool `json:"business"`
 	Stack         bool `json:"stack"`
 	Type          bool `json:"type,omitempty"`
@@ -56,66 +48,66 @@ type CodeNamePair struct {
 	Name string `json:"name"`
 }
 
-func call_metainfo_api() MetaInfoApiResponse {
-	// Create an instance of the request body
-	body := requestBody{
-		Business: true,
-		Stack:    true,
-		Region:   true,
-	}
-
-	// Marshal the request body into JSON
-	jsonBody, err := json.Marshal(body)
-	if err != nil {
-		fmt.Println("Error marshaling JSON:", err)
-		return MetaInfoApiResponse{}
-	}
-
-	// Define the API endpoint and token
-	url := "https://lark-oncall.bytedance.net/openapi/ticket/v1/getMetaInfo"
-	token := "26ad213fcdc54e0da3a6e7fc79e99b75"
-
-	// Create a new HTTP client
-	client := &http.Client{}
-
-	// Create a new request
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
-	if err != nil {
-		fmt.Println("Error creating request:", err)
-		return MetaInfoApiResponse{}
-	}
-
-	// Add headers
-	req.Header.Set("Authorization", "Basic "+token)
-	req.Header.Set("Content-Type", "application/json")
-
-	// Send the request
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return MetaInfoApiResponse{}
-	}
-	defer resp.Body.Close()
-
-	// Read the response body
-	respBody, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return MetaInfoApiResponse{}
-	}
-
-	// Print the response body
-	fmt.Println("Response:", string(respBody))
-
-	var response MetaInfoApiResponse
-	err = json.Unmarshal(respBody, &response)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return MetaInfoApiResponse{}
-	}
-
-	// Print the response struct to verify
-	fmt.Printf("%+v\n", response)
-
-	return response
-}
+//func call_metainfo_api() MetaInfoApiResponse {
+//	// Create an instance of the request body
+//	body := requestBody{
+//		Business: true,
+//		Stack:    true,
+//		Region:   true,
+//	}
+//
+//	// Marshal the request body into JSON
+//	jsonBody, err := json.Marshal(body)
+//	if err != nil {
+//		fmt.Println("Error marshaling JSON:", err)
+//		return MetaInfoApiResponse{}
+//	}
+//
+//	// Define the API endpoint and token
+//	url := "https://lark-oncall.bytedance.net/openapi/ticket/v1/getMetaInfo"
+//	token := "26ad213fcdc54e0da3a6e7fc79e99b75"
+//
+//	// Create a new HTTP client
+//	client := &http.Client{}
+//
+//	// Create a new request
+//	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonBody))
+//	if err != nil {
+//		fmt.Println("Error creating request:", err)
+//		return MetaInfoApiResponse{}
+//	}
+//
+//	// Add headers
+//	req.Header.Set("Authorization", "Basic "+token)
+//	req.Header.Set("Content-Type", "application/json")
+//
+//	// Send the request
+//	resp, err := client.Do(req)
+//	if err != nil {
+//		fmt.Println("Error sending request:", err)
+//		return MetaInfoApiResponse{}
+//	}
+//	defer resp.Body.Close()
+//
+//	// Read the response body
+//	respBody, err := ioutil.ReadAll(resp.Body)
+//	if err != nil {
+//		fmt.Println("Error reading response body:", err)
+//		return MetaInfoApiResponse{}
+//	}
+//
+//	// Print the response body
+//	fmt.Println("Response:", string(respBody))
+//
+//	var response MetaInfoApiResponse
+//	err = json.Unmarshal(respBody, &response)
+//	if err != nil {
+//		fmt.Println("Error unmarshaling JSON:", err)
+//		return MetaInfoApiResponse{}
+//	}
+//
+//	// Print the response struct to verify
+//	fmt.Printf("%+v\n", response)
+//
+//	return response
+//}
